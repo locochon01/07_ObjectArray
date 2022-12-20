8f0868bb67c40a9c27524e94c659e92a457d246a
locochon01 <beneplemx@gmail.com>
2022-12-20 11:10:38 +0900

    first commit

--- src/edu/kh/oarr/run/MemberRun.java
+++ src/edu/kh/oarr/run/MemberRun.java
@@ -1,9 +1,16 @@
 package edu.kh.oarr.run;
 
+import edu.kh.oarr.model.serivce.MemberService;
+
 public class MemberRun {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		MemberService ms = new MemberService();
+		
+		ms.displayMenu();
+		
+		
 
 	}
 
